5ccbbc8d6aa8e4df20435634a278dd3ad7b04fbb
Andre Cardoso <andrecardosodev@gmail.com>
2013-06-28 16:55:17 -0300

    Cópia da pasta coverage

--- init.php
+++ init.php
@@ -11,14 +11,36 @@ if ( isset($argv[1]) ) {
     $caminhoCopiaArquivos = $argv[1];
     
     
-    echo "\nCopiando arquivos do pacote base para o destino final \n\n";
+    echo "\nCOPIANDO ARQUIVOS DO PACOTE BASE PARA O DESTINO INFORMADO\n\n";
     
+    echo "\n-- Copiando Models\n";
     exec("cp -fr app/Model " . $caminhoCopiaArquivos . "/app/");
+    echo "\n-- Ok\n";
+    
+    echo "\n-- Copiando Controllers\n";
     exec("cp -fr app/Controller " . $caminhoCopiaArquivos . "/app/");
+    echo "\n-- Ok\n";
+    
+    echo "\n-- Copiando Views\n";
     exec("cp -fr app/View " . $caminhoCopiaArquivos . "/app/");
+    echo "\n-- Ok\n";
+    
+    echo "\n-- Copiando Css\n";
     exec("cp -fr app/webroot/css/* " . $caminhoCopiaArquivos . "/app/webroot/css/");
+    echo "\n-- Ok\n";
+    
+    echo "\n-- Copiando Js\n";
     exec("cp -fr app/webroot/js/* " . $caminhoCopiaArquivos . "/app/webroot/js/");
+    echo "\n-- Ok\n";
+    
+    echo "\n-- Copiando Coverage\n";
+    exec("cp -fr app/webroot/coverage " . $caminhoCopiaArquivos . "/app/webroot/");
+    echo "\n-- Ok\n";
+    
+    echo "\n-- Copiando Testes\n";
     exec("cp -fr app/Test " . $caminhoCopiaArquivos . "/app/");
+    echo "\n-- Ok\n";
+    
     
     echo "\nOk\n\n";
     
@@ -27,7 +49,7 @@ if ( isset($argv[1]) ) {
     echo "\nOk\n";
     
     
-    echo "Finalizado!";
+    echo "\n\nPROCESSO DE CÓPIA FINALIZADO!\n\n";
     
 } else {
     echo "\n\nInforme o caminho completo para que os arquivos sejam copiados Ex: '/var/www/meu-projeto'\n\n";
