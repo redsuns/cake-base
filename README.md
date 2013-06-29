d4021140d9d2ad8b49ce3e0654b1c278bc07dc47
Andre Cardoso <andrecardosodev@gmail.com>
2013-06-28 22:10:39 -0300

    Refatorado init.php e descrita utilização no README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+#Cake Base
 
 Base de features comuns em diversos projetos
+
+
+###Instalação
+
+* Crie um projeto baseado no CakePHP com a versão mais recente
+* atualize este repositório em seu localhost `git pull`
+* a partir do shell entre na pasta deste projeto
+* Execute: ` php init.php /caminho/completo/ate/seu/projeto/recem/criado `
+
+
+###Configuração
+
+Após a instalação suponho que ao criar seu projeto do CakePHP já tenha configurado o banco de dados default,
+caso não tenha configure-o agora e siga os passos abaixo:
+
+* a partir do shell entre na pasta raiz de seu novo projeto e em seguida entre na pasta ` app/Console ` 
+e rode o cake schema ` ./cake schema create ` para que seu banco de dados seja inicializado. 
+
+* Caso o shell do cake não possa ser executado importe o arquivo schema.sql de ` app/Config/Schema ` através de 
+seu gerenciador de banco de dados.
+
+
+
+Para mais opções execute o init.php sem parâmetro que um menu de ajuda será exibido.
+
--- init.php
+++ init.php
@@ -1,68 +1,169 @@
 <?php
 /**
- * Script para adicionar a base em um novo projeto
+ * Script para adicionar a base customizada do CakePHP em um novo projeto
  * 
- * v1
+ * @version 1 - 28/06/2013
+ * @author Andre Cardoso <andrecardosodev@gmail.com>
+ * 
  */
 
 
+$init = new InitBase();
+
 if ( isset($argv[1]) ) {
-  
-    $caminhoCopiaArquivos = $argv[1];
-    
-    
-    echo "\nCOPIANDO ARQUIVOS DO PACOTE BASE PARA O DESTINO INFORMADO\n\n";
-    
-    echo "\n-- Copiando Models\n";
-    exec("cp -fr app/Model " . $caminhoCopiaArquivos . "/app/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Controllers\n";
-    exec("cp -fr app/Controller " . $caminhoCopiaArquivos . "/app/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Views\n";
-    exec("cp -fr app/View " . $caminhoCopiaArquivos . "/app/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Css\n";
-    exec("cp -fr app/webroot/css/* " . $caminhoCopiaArquivos . "/app/webroot/css/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Js\n";
-    exec("cp -fr app/webroot/js/* " . $caminhoCopiaArquivos . "/app/webroot/js/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Coverage\n";
-    exec("cp -fr app/webroot/coverage " . $caminhoCopiaArquivos . "/app/webroot/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Testes\n";
-    exec("cp -fr app/Test " . $caminhoCopiaArquivos . "/app/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Plugins\n";
-    exec("cp -fr app/Plugin " . $caminhoCopiaArquivos . "/app/");
-    echo "\n-- Ok\n";
-    
-    echo "\n-- Copiando Schema e database model\n";
-    exec("cp -fr app/Config/Schema/base.mwb " . $caminhoCopiaArquivos . "/app/Config/Schema/");
-    exec("cp -fr app/Config/Schema/schema.php " . $caminhoCopiaArquivos . "/app/Config/Schema/");
-    echo "\n-- Ok\n";
-    
-    
-    
-    echo "\nOk\n\n";
-    
-    echo "Aplicando permissões necessárias \n\n";
-    exec("chmod -R 777 " . $caminhoCopiaArquivos);
-    echo "\nOk\n";
-    
-    
-    
-    echo "\n\nPROCESSO DE CÓPIA FINALIZADO!\n\n";
-    
+
+    switch ($argv[1]) {
+        case 'help':
+            $init->help();
+            break;
+        case 'version':
+            $init->version();
+            break;
+        default:
+            if ( is_dir($argv[1]) ) {
+                $init->execute($argv[1]);
+            } else {
+                echo "\nERRO: O caminho digitado '" . $argv[1] . "' não foi localizado\n";
+                echo "por favor informe um caminho válido\n";
+                $init->available();
+            }
+            break;
+    }
 } else {
-    echo "\n\nInforme o caminho completo para que os arquivos sejam copiados Ex: '/var/www/meu-projeto'\n\n";
+    $init->available();
 }
-exit();
+
+
+class InitBase {
+    
+    private $caminhoCopiaArquivos;
+    private $version;
+
+
+    /**
+     * 
+     * @param string $destino
+     */
+    public function __construct() {
+        $this->version = '1 - 28/06/2013';
+    }
+    
+    /**
+     * 
+     */
+    public function version() {
+        echo "\n\n-----------------------------------------\nCake Base Init versão " . $this->version . "\n-----------------------------------------\n\n";
+    }
+    
+    /**
+     * 
+     */
+    public function help() {
+        
+        $help = "\n\n-----------------------------------------";
+        $help .= "\n\n";
+        $help .= "Digite o caminho completo em que seu projeto se encontra";
+        $help .= "\npara copiar os arquivos base.";
+        $help .= "\n\n\n";
+        $help .= "-----------------------------------------";
+        $help .= "\n\n\n";
+           
+        echo $help;
+    }
+    
+    /**
+     * Exibido quando nada for digitado
+     */
+    public function available() {
+        echo "\nCOMANDOS DISPONÍVEIS\n";
+        echo "------------------------------------------------------------------------------";
+        echo "\n* help                -- Exibe a ajuda deste utilitário\n";
+        echo "* version             -- Exibe a versão deste utilitário\n";
+        echo "* /caminho/completo   -- Copia os arquivos base para o diretório fornecido\n";
+        echo "------------------------------------------------------------------------------\n\n";
+    }
+    
+    
+    
+    public function execute($destino) {
+        $this->caminhoCopiaArquivos = $destino;
+        
+        echo "\nEXECUTANDO CÓPIA E CONFIGURAÇÕES DO CAKE BASE\n\n";
+        
+        $this->__copiaArquivos();
+        $this->__habilitaPlugins();
+        $this->__aplicarPermissoes();
+        
+        echo "\nPROCESSO FINALIZADO!\n\n";
+    }
+    
+    
+    /**
+     * 
+     */
+    private function __copiaArquivos() {
+        
+        echo "\n-- Copiando Models ";
+        exec("cp -fr app/Model " . $this->caminhoCopiaArquivos . "/app/");
+        echo "-- Ok\n";
+
+        echo "\n-- Copiando Controllers ";
+        exec("cp -fr app/Controller " . $this->caminhoCopiaArquivos . "/app/");
+        echo "-- Ok\n";
+
+        echo "\n-- Copiando Views ";
+        exec("cp -fr app/View " . $this->caminhoCopiaArquivos . "/app/");
+        echo "-- Ok\n";
+
+        echo "\n-- Copiando Css ";
+        exec("cp -fr app/webroot/css/* " . $this->caminhoCopiaArquivos . "/app/webroot/css/");
+        echo "-- Ok\n";
+
+        echo "\n-- Copiando Js ";
+        exec("cp -fr app/webroot/js/* " . $this->caminhoCopiaArquivos . "/app/webroot/js/");
+        echo "-- Ok\n";
+
+        echo "\n-- Copiando Coverage ";
+        exec("cp -fr app/webroot/coverage " . $this->caminhoCopiaArquivos . "/app/webroot/");
+        echo " -- Ok\n";
+
+        echo "\n-- Copiando Testes ";
+        exec("cp -fr app/Test " . $this->caminhoCopiaArquivos . "/app/");
+        echo "-- Ok\n";
+
+
+        echo "\n-- Copiando Schema e database model ";
+        exec("cp -fr app/Config/Schema/base.mwb " . $this->caminhoCopiaArquivos . "/app/Config/Schema/");
+        exec("cp -fr app/Config/Schema/schema.php " . $this->caminhoCopiaArquivos . "/app/Config/Schema/");
+        exec("cp -fr app/Config/Schema/schema.sql " . $this->caminhoCopiaArquivos . "/app/Config/Schema/");
+        echo "-- Ok\n";
+
+    }
+    
+    
+    /**
+     * 
+     */
+    private function __habilitaPlugins() {
+        echo "\n-- Copiando Plugins ";
+        exec("cp -fr app/Plugin " . $this->caminhoCopiaArquivos . "/app/");
+        echo "-- Ok\n";
+        
+         echo "\n-- Habilitando plugins ";
+        $bootstrap = fopen($this->caminhoCopiaArquivos . "/app/Config/bootstrap.php", "a");
+        fwrite($bootstrap, "\nCakePlugin::loadAll();\n");
+        fclose($bootstrap);
+        echo "-- Ok\n";
+    }
+    
+    
+    /**
+     * 
+     */
+    private function __aplicarPermissoes() {
+        echo "\n-- Aplicando permissões ";
+        exec("chmod -R 777 " . $this->caminhoCopiaArquivos);
+        echo "-- Ok\n";
+    }
+    
+}
