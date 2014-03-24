50e2687974398b531d3ecb8e30235a30a07ed5d5
Andre Cardoso <andrecardosodev@gmail.com>
2014-03-24 14:10:35 -0300

    Melhorado SlugBehavior
    Agora não é mais preciso intervenção direta na model que deseja utilizar o slug
    basta que seja definido o campo ('field' => 'name') que o behavior realizar o tratamento correto

--- app/Model/Behavior/SlugBehavior.php
+++ app/Model/Behavior/SlugBehavior.php
@@ -10,22 +10,42 @@ class SlugBehavior extends ModelBehavior
 
     public $settings = array();
 
+    /**
+     * 
+     * @param Model $model
+     * @param array $config
+     */
     function setup(Model $model, $config = array())
     {
         $this->settings[$model->alias] = $config;
     }
 
+    
+    /**
+     * 
+     * @param Model $model
+     * @param array $options
+     * @return boolean
+     */
+    public function beforeSave(Model $model, $options = array()) 
+    {
+        if ( !isset($model->data[$model->alias]['id']) && !empty($this->settings[$model->alias]['field'])) {
+            $model->data[$model->alias]['slug'] = $this->_generateSlug($model, $this->settings[$model->alias]['field']);
+        }
+        return true;
+    }
+    
     /**
      * At the moment just on Create is able to parse currectly
      * the slug
      * 
      * @param Model $model
-     * @param string $string
+     * @param string $field
      * @return string
      */
-    public function generateSlug(Model $model, $string )
+    protected function _generateSlug(Model $model, $field )
     {
-        $slug = strtolower(Inflector::slug($string, '-'));
+        $slug = strtolower(Inflector::slug($model->data[$model->alias][$field], '-'));
             
         $lastSlug = $model->find('first', array(
             'conditions' => array($model->alias . '.slug LIKE' => $slug . '%'),
